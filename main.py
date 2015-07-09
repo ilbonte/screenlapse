eedbfe2e578e78fbd01bda5223a93bf3492b1988
ilbonte <davide.bonte@gmail.com>
2015-07-09 15:40:34 +0200

    add webcam image capture

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ __author__ = 'Bonte'
 
 import thread
 import time
+from VideoCapture import Device
 
 from PIL import ImageGrab
 
@@ -13,29 +14,45 @@ def input_thread(L):
     L.append(None)
 
 
-def do_print():
+def do_screen():
     L = []
     i = 0
+    thread.start_new_thread(input_thread, (L,))
+    while 1:
+        time.sleep(.5)
+        i = i + 1
+        istr = str(i)
+        ImageGrab.grab().save("imgs/" + istr + ".png", "PNG")
+        print "saved"
+        if L:
+            main()
+            break
+
+
+def do_cam():
+    L = []
+    i = 0
+    cam = Device()
     thread.start_new_thread(input_thread, (L,))
     while 1:
         time.sleep(1)
         i = i + 1
         istr = str(i)
-        ImageGrab.grab().save("imgs/" + istr + ".png", "PNG")
 
+        cam.saveSnapshot("imgs/" + istr + ".png")
         print "saved"
         if L:
             main()
             break
-        print "Hi Mom!"
 
 
 def main():
-    choice = int(raw_input("Cosa vuoi? 0 per printare"))
+    choice = int(raw_input("Scegli cosa vuoi fare: \n 0 per printare\n 1 per camera"))
     if choice == 0:
-        do_print()
+        do_screen()
+    elif choice == 1:
+        do_cam()
     else:
         print "nada"
-
 
 main()
