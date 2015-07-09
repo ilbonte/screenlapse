6d1636af70ebebcdf3ec410d119a25f4f03c7187
ilbonte <davide.bonte@gmail.com>
2015-07-09 16:55:39 +0200

    created combined image

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import time
 from VideoCapture import Device
 
 from PIL import ImageGrab
+from PIL import Image
+import pygame
 
 choice = 0
-
+pygame.init()
+width = pygame.display.Info().current_w
+height = pygame.display.Info().current_h
 
 def input_thread(L):
     raw_input()
@@ -40,19 +44,50 @@ def do_cam():
         istr = str(i)
 
         cam.saveSnapshot("imgs/" + istr + ".png")
+
         print "saved"
         if L:
             main()
             break
 
 
+def do_both():
+    L = []
+    i = 0
+    cam = Device()
+    thread.start_new_thread(input_thread, (L,))
+    photo = cam.getImage(3, 1, "tl")
+    photoWidth, phoroHeight = photo.size
+    combinedImage = Image.new("RGB", (width + photoWidth, height), color=0)
+    while 1:
+        time.sleep(1)
+        i = i + 1
+        istr = str(i)
+
+        combinedImage.paste(ImageGrab.grab(), (0, 0))
+        photo = cam.getImage(3, 1, "tl")
+        combinedImage.paste(photo, (width, 0))
+        # ImageGrab.grab().save("imgs/screen" + istr + ".png", "PNG")
+        # cam.saveSnapshot("imgs/" + istr + ".png")
+        combinedImage.save("imgs/" + istr + ".png")
+        print "saved"
+        if L:
+            main()
+            break
+
+
+
 def main():
-    choice = int(raw_input("Scegli cosa vuoi fare: \n 0 per printare\n 1 per camera"))
-    if choice == 0:
+    choice = int(raw_input("Scegli cosa vuoi fare: \n 1 per printare\n 2 per camera \n 3 combined \n 0 per uscire"))
+    if choice == 1:
         do_screen()
-    elif choice == 1:
+    elif choice == 2:
         do_cam()
+    elif choice == 3:
+        do_both()
+    elif choice == 0:
+        print "Ciao! :D"
     else:
-        print "nada"
+        print "Opzione non valida"
 
 main()
